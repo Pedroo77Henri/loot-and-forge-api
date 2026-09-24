e41c0d69d2f79b48f9baa6f47edfd5c345b7d2a2
Pedroo77Henri <pedro.henrique.evangelista77@gmail.com>
2026-09-24 18:15:11 -0300

    feat: add services and repository layers (MVC refactoring)

--- app/services.py
+++ app/services.py
@@ -10,3 +10,23 @@ def create_item(db, payload):
         status=Status.READY
     )
     return repository.create_item(db, new_item)
+
+def forge_item(db, payload):
+    forge_item = Item(
+        name=payload.name,
+        time_required=payload.time_required,
+        status=Status.FORGING,
+        item_type=payload.item_type,
+        rarity=payload.rarity,
+        power=None
+    )
+    return repository.forge_item(db, forge_item)
+
+def get_all_items(db, item_type=None, item_rarity=None, order_by=None, direction=None):
+    return repository.get_all_items(db, item_type, item_rarity, order_by, direction)
+
+def get_item_by_id(db, item_id: int):
+    return repository.get_item_by_id(db, item_id)
+
+def delete_item(db, payload):
+    return repository.delete_item(db, payload)
